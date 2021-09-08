9cd6b5f70a5ea8725b248d49a1e9126552a1501c
William Guimont-Martin <wil9876543210@hotmail.com>
2021-09-08 10:42:03 -0400

    setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='pipeline',
+    version='1.0',
+    author='William Guimont-Martin',
+    author_email='william.guimont-martin.1@ulaval.ca',
+    description='Some transforms utilities based on `Arrows` from Haskell',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/willGuimont/pipeline',
+    project_urls = {
+        "Bug Tracker": "https://github.com/willGuimont/pipeline/issues"
+    },
+    license='MIT',
+    packages=['pipeline'],
+    install_requires=[''],
+)
